747f17fe4d5f5e627bd38e4776113fac60b5019b
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-04-27 19:24:15 +0530

    Add files via upload

---
+++ Pattern Printing/Pattern Of Left Lower Trangle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i=0,j=0,x=0;
+    
+    printf("\n Enter Numbers Of Rows And Colums =>");
+    scanf("%d",&x);
+    
+    for(i=1;i<=x;i++)
+    {
+        for(j=1;j<=x;j++)
+        {
+            if(i>=j)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
